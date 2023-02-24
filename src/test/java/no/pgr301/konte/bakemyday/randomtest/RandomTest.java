b0c945e71b330082b00ebbc9f689b9d8fabdb0b4
q10357 <isch004@student.kristiania.no>
2023-02-24 16:20:10 +0100

    ci.yml configures to run jobs on every pull_request. Added failing test

---
+++ src/test/java/no/pgr301/konte/bakemyday/randomtest/RandomTest.java
@@ -0,0 +1,14 @@
+package no.pgr301.konte.bakemyday.randomtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class RandomTest {
+
+    @Test
+    public void testValueNotNull() {
+        Integer notNull = null;
+        assertNotNull(notNull);
+    }
+}
