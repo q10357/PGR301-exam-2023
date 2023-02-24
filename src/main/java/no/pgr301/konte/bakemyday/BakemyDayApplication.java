89553d9dcaa093862a15c0ca269503fde1f05607
q10357 <isch004@student.kristiania.no>
2023-02-24 15:56:51 +0100

    Commit all initial files

---
+++ src/main/java/no/pgr301/konte/bakemyday/BakemyDayApplication.java
@@ -0,0 +1,13 @@
+package no.pgr301.konte.bakemyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakemyDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakemyDayApplication.class, args);
+	}
+
+}
