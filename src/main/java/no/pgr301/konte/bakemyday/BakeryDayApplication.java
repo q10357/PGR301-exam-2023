4d4878df7962a7ede4483afb58ea2b3f50de5f2d
q10357 <isch004@student.kristiania.no>
2023-02-24 17:16:16 +0100

    Changes to test, now passing

--- src/main/java/no/pgr301/konte/bakemyday/BakeryDayApplication.java
+++ src/main/java/no/pgr301/konte/bakemyday/BakeryDayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BakemyDayApplication {
+public class BakeryDayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BakemyDayApplication.class, args);
+		SpringApplication.run(BakeryDayApplication.class, args);
 	}
 
 }
